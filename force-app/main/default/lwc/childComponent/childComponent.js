920c0e54c61c16c9ccfa36eecdd084e08622681a
PranitTelore <pranit.telore1@gmail.com>
2023-08-23 16:17:38 +0530

    Decorator @Track

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,5 +2,9 @@ import { LightningElement, api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
     @api
-    message='Hello Child'
+    message='Hello Child';
+    @api
+    childFunction(){
+        console.log('Child Function called');
+    }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ParentComponent extends LightningElement {}
+export default class TrackComponent extends LightningElement {
+    
+    @track
+    fullName = {
+        firstName: "First Name",
+        lastName: "Last Name"
+    }
+
+    handleInput(event) {
+        console.log('Handle input is running');
+        if(event.target.name == 'firstName') {
+            this.fullName.firstName = event.target.value;
+        }
+
+        else if(event.target.name == 'lastName') {
+            this.fullName.lastName = event.target.value;
+        }
+
+        console.log('fullName property value : '+JSON.stringify(this.fullName));
+    }
+}
