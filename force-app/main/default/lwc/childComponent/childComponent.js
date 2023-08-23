fab89fa4195f9f50af843034e293c80c1e7c257b
PranitTelore <pranit.telore1@gmail.com>
2023-08-23 15:27:05 +0530

    Decorator @Api use

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ChildComponent extends LightningElement {}
+export default class ChildComponent extends LightningElement {
+    @api
+    message='Hello Child'
+}
